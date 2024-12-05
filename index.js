5a23e58bdbaba7741769da9193e7e8b3ec3ff6a0
rowperpetua <elaperpetua11@gmail.com>
2024-12-06 07:28:30 +0800

    jay progress

---
+++ index.js
@@ -0,0 +1,15 @@
+//Navbar
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll", ()=> 
+    navbar.classList.toggle("sticky", window.scrollY >0)
+);
+
+const menu = document.querySelector(".menu");
+const toggleMenu = () => menu.classList.toggle("active");
+
+document.querySelector(".menu-btn").addEventListener("click", toggleMenu);
+document.querySelector(".close-btn").addEventListener("click", toggleMenu);
+
+document
+    .querySelector("menu.a");
+    .forEach(link) => link.addEventListener("click",toggleMenu);
