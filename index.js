331cb1f570a45c7ee13024624ad54ed08475face
rowperpetua <elaperpetua11@gmail.com>
2024-12-07 00:00:26 +0800

    updated js for scrolling

--- index.js
+++ index.js
@@ -12,4 +12,17 @@ document.querySelector(".close-btn").addEventListener("click", toggleMenu);
 
 document
     .querySelector("menu.a");
-    .forEach(link) => link.addEventListener("click",toggleMenu);
+    .forEach((link) => link.addEventListener("click",toggleMenu));
+
+//LENIS FOR SMOOTH SCROLLING
+const lenis = new Lenis();
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
